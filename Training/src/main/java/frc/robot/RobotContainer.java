0f348e03af8cfccfc0c04fa56b077fc8c36e4c64
AlmaKaplan <alma20062010@gmail.com>
2025-07-29 11:24:49 +0300

    subsystem intake

--- Training/src/main/java/frc/robot/RobotContainer.java
+++ Training/src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Commands.IntakeCommand;
+
 
 public class RobotContainer {
   public static final PS5Controller controller = new PS5Controller(0);
@@ -15,8 +15,7 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    new Trigger(() -> controller.getTriangleButton()) 
-    .onTrue(new IntakeCommand());
+
   }
 
   public Command getAutonomousCommand() {
--- Training/src/main/java/frc/robot/Subsystem/Intake/IntakeConstants.java
+++ Training/src/main/java/frc/robot/Subsystem/Intake/IntakeConstants.java
@@ -4,5 +4,7 @@ public class IntakeConstants {
 
     public static final double GEAR = 1/5;
 
-    
+    public static final int RIGHT_MOTOR_ID = 16;
+    public static final int LEFT_MOTOR_ID = 18;
+
 }
